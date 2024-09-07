81b43c8c7cf4c4cb25ee98d64cabcb2881e38f36
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-09-07 17:03:22 +0530

    code

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:9005529898@cluster0.dambm29.mongodb.net/food-del').then(() => console.log("Connect DB"));
-}
-
